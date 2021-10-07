012a48c6830ead1f95e05d0e79b8abdab575dffd
ohmnapt <napats.work@gmail.com>
2021-10-08 01:22:49 +0700

    create set timeout to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,10 +19,11 @@ const Search = () => {
 
             setResults(data.query.search);
         }
-
-        if(term){
-            search();
-        }
+        const timeoutId = setTimeout(() => {
+            if(term){
+                search();
+            }
+        }, 500);
     }, [term]);
 
     const rendererResults =  results.map((result) => {
