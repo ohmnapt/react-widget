604d711167d2ee0be8e9b62302377bb9c116838b
ohmnapt <napats.work@gmail.com>
2021-10-10 03:03:15 +0700

    add clearTimeout func & set search when refresh

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,33 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const Accordion = ({items}) => {
-    const [activeIndex, setActiveIndex] = useState(null);
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-    const onTitleClick = (index) => {
-        setActiveIndex(index);
-    }
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
 
-    const rendererItems = items.map((item, index) => {
-        const active = index === activeIndex ? 'active' : '';
+  const rendererItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
 
-        return (
-        <React.Fragment key={item.title}>
-            <div className={`title ${active}`}
-                onClick={() => onTitleClick(index)}
-            >
-                <i className="dropdown icon"></i>
-                {item.title}
-            </div>
-            <div className={`content ${active}`}>
-                <p>{item.content}</p>
-            </div>
-        </React.Fragment>
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
     );
-});
+  });
 
-    return <div className="ui styled accordion">
-        {rendererItems}
-    </div>
+  return <div className="ui styled accordion">{rendererItems}</div>;
 };
 
 export default Accordion;
+
+const a = () => {};
