dd8045d7285c76039e4231ec117e199625f082e6
ohmnapt <napats.work@gmail.com>
2021-10-10 03:55:45 +0700

    create dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
-    {
-        title: 'What is React',
-        content: 'React is a front end javascript framework'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favorite JS library among engineers'
-    },
-    {
-        title: 'How do you use React',
-        content: 'You use React bt create components'
-    },
+  {
+    title: "What is React",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React",
+    content: "You use React bt create components",
+  },
+];
+
+const options = [
+  {
+    label: "The color Red",
+    value: "Red",
+  },
+  {
+    label: "The color Green",
+    value: "Green",
+  },
+  {
+    label: "The color Blue",
+    value: "Blue",
+  },
 ];
 
 export default () => {
-    return <div>
-        <Search/>
-    </div>;
+  return (
+    <div>
+      <Dropdown options={options} />
+    </div>
+  );
 };
